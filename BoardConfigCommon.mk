078605d15baee20ecf07136e18e9bb20fed16fbe
Arijit78 <sahaarijit2007@gmail.com>
2024-03-09 07:38:02 +0000

    sm6375: Fixup prebuilt kernel
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,6 +98,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc LLVM=1
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6375
+TARGET_KERNEL_CONFIG := holi_QGKI
 
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1
